3945d36ef5f8549d2a2d02c03c7dcb68318c74c6
Deepak bairwa <86884073+dkgangawat@users.noreply.github.com>
2024-01-08 16:21:39 +0530

    ..

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const manifestForPlugin = {
+  registerType: "autoUpdate",
+  includeAssets: [
+    "favicon.ico",
+    "/android-chrome-192x192.png",
+    "apple-touch-icon.png",
+  ],
+
+  manifest: {
+    name: "admin Dashboard",
+    short_name: "Dashboard",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+  },
+  theme_color: "#ffffff",
+  background_color: "#ffffff",
+  display: "standalone",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
