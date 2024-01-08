48781bf7a3ba1e9cd6f9625222e98545199ac42e
Deepak bairwa <86884073+dkgangawat@users.noreply.github.com>
2024-01-09 02:23:54 +0530

    logout button

--- client/src/components/common/SideBar.jsx
+++ client/src/components/common/SideBar.jsx
@@ -7,6 +7,7 @@ import { CiFileOn } from "react-icons/ci";
 import { IoIosArrowForward } from "react-icons/io";
 import { TfiDashboard } from "react-icons/tfi";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 const SideBar = () => {
   const dispatch = useDispatch();
 
@@ -14,6 +15,11 @@ const SideBar = () => {
     { name: "Dashboard", icon: <TfiDashboard />, to: "/" },
     { name: "Data", icon: <CiFileOn />, to: "/data" },
   ];
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    window.location.reload();
+  };
 
   return (
     <>
@@ -33,28 +39,31 @@ const SideBar = () => {
             </IconButton>
           </div>
         </div>
-        {/* links in sidebar with icons and text */}
+        <div className=" flex flex-col justify-between h-full">
+          {/* links in sidebar with icons and text */}
 
-        <div className=" mt-4 text-gray-500">
-          {links.map((link, index) => (
-            <Link
-              to={link.to}
-              key={index}
-              className=" flex items-center space-x-2  relative my-2 rounded-md transition-all duration-300 ease-in-out hover:bg-gray-100 "
-            >
-              <div className=" text-2xl p-2 font-light">{link.icon}</div>
-              <div className=" text-sm">{link.name}</div>
-              <IoIosArrowForward className=" absolute right-2 text-gray-300" />
-            </Link>
-          ))}
-        </div>
+          <div className=" mt-4 text-gray-500">
+            {links.map((link, index) => (
+              <Link
+                to={link.to}
+                key={index}
+                className=" flex items-center space-x-2  relative my-2 rounded-md transition-all duration-300 ease-in-out hover:bg-gray-100 "
+              >
+                <div className=" text-2xl p-2 font-light">{link.icon}</div>
+                <div className=" text-sm">{link.name}</div>
+                <IoIosArrowForward className=" absolute right-2 text-gray-300" />
+              </Link>
+            ))}
+          </div>
 
-        {/* logout button */}
-        {/* <div className=" absolute bottom-0 left-0 w-full p-4 ">
-          <button className=" w-full bg-blue-900 text-white rounded-md py-2 px-4 hover:bg-blue-800 transition-all duration-300 ease-in-out">
+          {/* logout button */}
+          <button
+            onClick={handleLogout}
+            className=" bg-gray-100 w-full text-gray-500 mb-16 rounded-md py-2 px-4 hover:bg-gray-200 transition-all duration-300 ease-in-out"
+          >
             Logout
           </button>
-        </div> */}
+        </div>
       </div>
     </>
   );
